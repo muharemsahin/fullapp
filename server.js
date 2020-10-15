dc3a1df979499843f28042fb3e529c77966c459e
Muharem <muharem.sahinovic@live.com>
2020-10-15 23:10:12 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express =require("express");
+const app = express();
+
+app.set("view-engine","ejs");
+
+app.get("/", (req,res)=>{
+    res.send("Ok");
+})
+
+app.listen(3000, function(){
+    console.log("Listening to port 3000");
+})
